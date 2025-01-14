a4ca1af0dcb2c04e8b057a8c8511eb20815e588a
Dhiraj Kumar Bhakta <bhaktadhiraj@gmail.com>
2025-01-14 13:29:18 +0530

    added bullBoardUI

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,16 @@ import serverConfig from './config/serverConfig';
 import sampleQueueProducer from "./producers/sampleQueueProducer";
 import apiRouter from './routes';
 import SampleWorker from "./workers/SampleWorker";
-
+import bullBoardAdapter from "./config/bullBoardConfig";
 
 const app = express();
 
-app.use('/api', apiRouter)
+app.use('/api', apiRouter);
+app.use('/ui', bullBoardAdapter.getRouter());
 
 app.listen(serverConfig.PORT, () => {
     console.log(`Server started at *:${serverConfig.PORT}`);
+    console.log(`BullBoard dashboard running on: http://localhost:${serverConfig.PORT}/ui`);
     SampleWorker('SampleQueue');
     sampleQueueProducer('SampleJob', {
       name: "Dhiraj",
