3963f80645b7b7e3385a43ece793947a9e385219
Dhiraj Kumar Bhakta <bhaktadhiraj@gmail.com>
2025-01-14 12:06:18 +0530

    Bull mq based redis queue infra up and runnig

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import express from 'express';
 import serverConfig from './config/serverConfig';
+import sampleQueueProducer from "./producers/sampleQueueProducer";
 import apiRouter from './routes';
+import SampleWorker from "./workers/SampleWorker";
+
 
 const app = express();
 
 app.use('/api', apiRouter)
 
-app.listen(serverConfig.PORT,()=>{
+app.listen(serverConfig.PORT, () => {
     console.log(`Server started at *:${serverConfig.PORT}`);
-});
+    SampleWorker('SampleQueue');
+    sampleQueueProducer('SampleJob', {
+      name: "Dhiraj",
+      company: "Microsoft",
+      position: "SDE 1 L61",
+      locatiion: "Remote | BLR | Noida"
+    });
+  });
--- src/jobs/SampleJob.ts
+++ src/jobs/SampleJob.ts
@@ -2,22 +2,26 @@ import {Job} from 'bullmq';
 
 import { IJob } from '../types/bullMqJobDefiniton';
 
-export default class SampleJob implements IJob{
-    name : string
-    payload : Record<string,unknown>
-    constructor(payload : Record<string,unknown>){
+export default class SampleJob implements IJob {
+    name: string;
+    payload: Record<string, unknown>;
+    constructor(payload: Record<string, unknown>) {
         this.payload = payload;
         this.name = this.constructor.name;
     }
 
-    handle = ()=>{
+    handle = (job?: Job) => {
         console.log("Handler of the job called");
-
-    };
-    failed =(job?:Job) : void =>{
-        console.log("Job failed");
-        if(job){
-            console.log(job.id);
+        console.log(this.payload);
+        if(job) {
+            console.log(job.name, job.id, job.data);
         }
     };
-}
+
+    failed = (job?: Job) : void => {
+        console.log("Job failed");
+        if(job) {
+            console.log(job.id);
+        }
+    }
+    };
